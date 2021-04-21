52cb4a386147b4979d9d4e0e6a1d6965a8790e0a
EdwardNee <nieduard_01@mail.ru>
2021-04-22 02:00:50 +0300

    fragment_create.xml added with some images in SVG

---
+++ app/src/main/java/com/bldj/project/CreateFragment.kt
@@ -0,0 +1,24 @@
+package com.bldj.project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Фрагмент окна создания объявления.
+ */
+class CreateFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create, container, false)
+    }
+}
