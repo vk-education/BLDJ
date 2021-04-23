7f792112dcb43f16cd3efba61bbe5754d84ade68
EdwardNee <nieduard_01@mail.ru>
2021-04-23 11:39:58 +0300

    BottomSheetCreateFragment.kt appears in CreateFragment.kt

--- app/src/main/java/com/bldj/project/CreateFragment.kt
+++ app/src/main/java/com/bldj/project/CreateFragment.kt
@@ -1,10 +1,15 @@
 package com.bldj.project
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 /**
  * Фрагмент окна создания объявления.
@@ -18,7 +23,14 @@ class CreateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_create, container, false)
+
+        val publishBtn = view.findViewById<Button>(R.id.create_ad)
+        publishBtn?.setOnClickListener {
+            val bottomSheet = BottomSheetCreateFragment()
+            bottomSheet.show((activity as FragmentActivity).supportFragmentManager, "TAG")
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false)
+        return view
     }
 }
--- app/src/main/java/com/bldj/project/MainActivity.kt
+++ app/src/main/java/com/bldj/project/MainActivity.kt
@@ -2,10 +2,36 @@ package com.bldj.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.security.InvalidParameterException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_layout)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null)
+            moveToFragment(AdsFragment())
+
+        val navigationBar = findViewById<BottomNavigationView>(R.id.nav_bar)
+        //Set the click listener on nav bar.
+        navigationBar.setOnNavigationItemSelectedListener {
+            val frag = when (it.itemId) {
+                R.id.home -> AdsFragment()
+                R.id.add -> CreateFragment()
+                else -> throw InvalidParameterException()
+            }
+            moveToFragment(frag)
+            true
+        }
+    }
+    /**
+     * Begins transaction to a selected on a navigation bar fragment.
+     */
+    private fun moveToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_wrapper, fragment)
+            .commit()
     }
 }
