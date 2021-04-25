5d269866160b5cbf9af69faa574a13d033906f65
EdwardNee <nieduard_01@mail.ru>
2021-04-25 12:10:48 +0300

    requireFragmentManager() in CreateFragment.kt, xml code arranged, unused imports, build.gradle legacy deleted

--- app/src/main/java/com/bldj/project/BottomSheetCreateFragment.kt
+++ app/src/main/java/com/bldj/project/BottomSheetCreateFragment.kt
@@ -1,8 +1,6 @@
 package com.bldj.project
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/bldj/project/CreateFragment.kt
+++ app/src/main/java/com/bldj/project/CreateFragment.kt
@@ -1,15 +1,11 @@
 package com.bldj.project
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
-import androidx.fragment.app.FragmentActivity
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import androidx.fragment.app.Fragment
 
 /**
  * Фрагмент окна создания объявления.
@@ -29,8 +25,9 @@ class CreateFragment : Fragment() {
         val publishBtn = view.findViewById<Button>(R.id.create_ad)
         publishBtn?.setOnClickListener {
             val bottomSheet = BottomSheetCreateFragment()
-            bottomSheet.show((activity as FragmentActivity).supportFragmentManager, "TAG")
+            bottomSheet.show(requireFragmentManager(), "TAG")
         }
+
         // Inflate the layout for this fragment
         return view
     }
