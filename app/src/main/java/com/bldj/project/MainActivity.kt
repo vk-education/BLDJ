6f772accbb580efba069763e48741c14a5b71f00
EdwardNee <nieduard_01@mail.ru>
2021-05-29 17:11:02 +0300

    cleanup

--- app/src/main/java/com/bldj/project/MainActivity.kt
+++ app/src/main/java/com/bldj/project/MainActivity.kt
@@ -1,25 +1,18 @@
 package com.bldj.project
 
-import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import data.IBackButton
-import data.User
 import java.security.InvalidParameterException
-import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
         val isNightModeOn: Boolean = appSettingPrefs.getBoolean("NightMode", false)
-        if(isNightModeOn){
+        if (isNightModeOn) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -47,15 +40,9 @@ class MainActivity : AppCompatActivity() {
             database = FirebaseDatabase.getInstance()
             usersDbRef = database?.reference?.child("users")
             usersChildEventListener = object : ChildEventListener {
-                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {}
 
-                }
-
-                override fun onChildChanged(
-                    snapshot: DataSnapshot,
-                    previousChildName: String?
-                ) {
-                }
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
 
                 override fun onChildRemoved(snapshot: DataSnapshot) {}
                 override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
