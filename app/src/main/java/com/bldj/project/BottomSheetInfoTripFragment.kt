ba2895599c47386be42a64237717c936050b2042
Даниил Ефимов <daefimov_1@edu.hse.ru>
2021-06-01 15:13:24 +0300

    две страницы информации

---
+++ app/src/main/java/com/bldj/project/BottomSheetInfoTripFragment.kt
@@ -0,0 +1,22 @@
+package com.bldj.project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class BottomSheetInfoTripFragment : BottomSheetDialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_info_trip, container, false)
+    }
+}
